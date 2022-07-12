3b1735710a8eb75e920082733b78908951ee9c35
Tom Fay <tomfay@microsoft.com>
2022-07-12 16:04:29 +0100

    initial commit of go-rustaudit

---
+++ rustaudit_test.go
@@ -0,0 +1,22 @@
+package rustaudit
+
+import (
+	"github.com/stretchr/testify/assert"
+	"log"
+	"os"
+	"testing"
+)
+
+func TestLinuxRustDependencies(t *testing.T) {
+	// Generate this with `DOCKER_BUILDKIT=1 docker build -f test/Dockerfile -o . .`
+	r, err := os.Open("hello-auditable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	versionInfo, err := GetDependencyInfo(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	assert.Equal(t, 19, len(versionInfo.Packages))
+	assert.Equal(t, Package{Name: "adler", Version: "1.0.2", Source: "registry", Kind: "build"}, versionInfo.Packages[0])
+}
