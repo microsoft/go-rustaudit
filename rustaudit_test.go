95d6d39b54bc7a07d39a51243f032a9314b848b1
Tom Fay <tomfay@microsoft.com>
2022-08-03 09:29:24 +0100

    update test fixture (#4)

--- rustaudit_test.go
+++ rustaudit_test.go
@@ -1,15 +1,16 @@
 package rustaudit
 
 import (
-	"github.com/stretchr/testify/assert"
 	"log"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLinuxRustDependencies(t *testing.T) {
 	// Generate this with `DOCKER_BUILDKIT=1 docker build -f test/Dockerfile -o . .`
-	r, err := os.Open("hello-auditable")
+	r, err := os.Open("crate_with_features_bin")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -17,6 +18,6 @@ func TestLinuxRustDependencies(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	assert.Equal(t, 19, len(versionInfo.Packages))
-	assert.Equal(t, Package{Name: "adler", Version: "1.0.2", Source: "registry", Kind: "build"}, versionInfo.Packages[0])
+	assert.Equal(t, 2, len(versionInfo.Packages))
+	assert.Equal(t, Package{Name: "crate_with_features", Version: "0.1.0", Source: "local", Kind: "runtime", Features: []string{"default", "library_crate"}, Dependencies: []uint{1}}, versionInfo.Packages[0])
 }
